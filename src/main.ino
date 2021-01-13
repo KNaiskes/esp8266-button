c3e2bb82e7b1956dc3e95e76e6534016e11ede21
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-01-13 21:36:05 +0200

    Add ESP8266WiFi settings

--- src/main.ino
+++ src/main.ino
@@ -1,4 +1,12 @@
+#include "ESP8266WiFi.h"
+
+const char* ssid     = "ssid";
+const char* password = "password";
+
 void setup() {
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) { delay(500); }
+
   pinMode(D1, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 }
