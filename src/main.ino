244c1d4c2ff640385038e3216edeed0d91dd325a
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-01-14 20:34:34 +0200

    Configure hostname

--- src/main.ino
+++ src/main.ino
@@ -1,9 +1,11 @@
 #include "ESP8266WiFi.h"
 
-const char* ssid     = "ssid";
-const char* password = "password";
+const char* ssid     = "ssid
+const char* password = "password
+const char* hostname = "esp8266";
 
 void setup() {
+  WiFi.hostname(hostname);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) { delay(500); }
 
