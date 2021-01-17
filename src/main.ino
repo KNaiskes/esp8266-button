01dcd3a178091386b596b108475cb40e9b959db0
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-01-17 13:45:59 +0200

    Add relayFlag variable in order to toggle state

--- src/main.ino
+++ src/main.ino
@@ -4,6 +4,8 @@ const char* ssid     = "ssid";
 const char* password = "password";
 const char* hostname = "esp8266";
 
+byte relayFlag = HIGH;
+
 void setup() {
   WiFi.hostname(hostname);
   WiFi.begin(ssid, password);
@@ -11,14 +13,21 @@ void setup() {
 
   pinMode(D1, INPUT);
   pinMode(D2, OUTPUT);
+
+  digitalWrite(D2, HIGH); // Default - starting state
 }
 
 void loop() {
   byte buttonInp = digitalRead(D1);
 
   if (buttonInp == HIGH) {
-    digitalWrite(D2, LOW);
-  } else {
-    digitalWrite(D2, HIGH);
+    if (relayFlag == LOW) {
+      relayFlag = HIGH;
+      digitalWrite(D2, HIGH);
+    } else {
+      relayFlag = LOW;
+      digitalWrite(D2, LOW);
+    }
+    delay(1000);
   }
 }
